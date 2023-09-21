1cdeb8f82cc34278cd1aebe5f4dba582393562ef
tpjigar <tp.jigar@gmail.com>
2023-09-21 19:36:44 +0530

    Customer, Category, Order modules

---
+++ app/Filament/Resources/CategoryResource.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\CategoryResource\Pages;
+use App\Filament\Resources\CategoryResource\RelationManagers;
+use App\Models\Category;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class CategoryResource extends Resource
+{
+    protected static ?string $model = Category::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-tag';
+
+    protected static ?int $navigationSort=4;
+    protected static ?string $navigationGroup='Shop';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Group::make()
+                ->schema([
+                    Forms\Components\Section::make()
+                    ->schema([
+                        Forms\Components\TextInput::make('name')
+                            ->required()
+                            ->unique(Category::class, 'name', ignoreRecord: true)
+                            ->live(onBlur: true)
+                            ->afterStateUpdated(function (string $operation, $state, Forms\Set $set){
+                                if($operation != 'create'){
+                                    return;
+                                }
+                                $set('slug', \Str::slug($state));
+                            }),
+
+                        Forms\Components\TextInput::make('slug')
+                            ->disabled()
+                            ->dehydrated()
+                            ->required()
+                            ->unique(Category::class, 'slug', ignoreRecord: true),
+
+                        Forms\Components\MarkdownEditor::make('description')
+                            ->columnSpanFull(),
+                    ])->columns(2),
+                ]),
+                Forms\Components\Group::make()
+                    ->schema([
+                        Forms\Components\Section::make('Status')
+                            ->schema([
+                                Forms\Components\Toggle::make('is_visible')
+                                    ->label('Visibility')
+                                    ->helperText('Enable or Disable category visibility')
+                                    ->default(true),
+                                Forms\Components\Select::make('parent_id')
+                                    ->label('Parent')
+                                    ->relationship('parent', 'name'),
+                            ])
+                    ]),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')->sortable()->searchable(),
+                Tables\Columns\TextColumn::make('parent.name')->label('Parent')->sortable()->searchable(),
+                Tables\Columns\IconColumn::make('is_visible')->label('Visibility')->boolean()->sortable(),
+                Tables\Columns\TextColumn::make('updated_at')->label('Updated Date')->date()->sortable()
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\ActionGroup::make([
+                    Tables\Actions\ViewAction::make(),
+                    Tables\Actions\EditAction::make(),
+                    Tables\Actions\DeleteAction::make(),
+                ])
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListCategories::route('/'),
+            'create' => Pages\CreateCategory::route('/create'),
+            'edit' => Pages\EditCategory::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Filament/Resources/OrderResource.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Enums\OrderStatusEnum;
+use App\Filament\Resources\OrderResource\Pages;
+use App\Filament\Resources\OrderResource\RelationManagers;
+use App\Models\Order;
+use App\Models\Product;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class OrderResource extends Resource
+{
+    protected static ?string $model = Order::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-shopping-bag';
+
+    protected static ?int $navigationSort = 3;
+    protected static ?string $navigationGroup = 'Shop';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Wizard::make([
+                    Forms\Components\Wizard\Step::make('Order Details')
+                    ->schema([
+                        Forms\Components\TextInput::make('number')
+                            ->required()->disabled()->dehydrated()->default('OR-'.rand(10000, 999999)),
+                        Forms\Components\Select::make('customer_id')
+                            ->required()
+                            ->searchable()
+                            ->relationship('customer', 'name'),
+                        Forms\Components\Select::make('status')
+                            ->options([
+                                'pending' => OrderStatusEnum::PENDING->value,
+                                'processing' => OrderStatusEnum::PROCESSING->value,
+                                'completed' => OrderStatusEnum::COMPLETED->value,
+                                'declined' => OrderStatusEnum::DECLINED->value,
+                            ])->required(),
+                        Forms\Components\MarkdownEditor::make('notes')->columnSpanFull(),
+                    ])->columns(3),
+
+                    Forms\Components\Wizard\Step::make('Order Items')
+                    ->schema([
+                        Forms\Components\Repeater::make('Items')
+                            ->relationship()
+                            ->schema([
+                                Forms\Components\Select::make('product_id')
+                                    ->label('Product')
+                                    ->options(Product::query()->pluck('name', 'id')),
+                                Forms\Components\TextInput::make('quantity')
+                                    ->numeric()->default(1)->required(),
+                                Forms\Components\TextInput::make('unit_price')
+                                    ->required()->numeric()->disabled()->dehydrated(),
+                            ])->columns(3),
+                    ]),
+                ])->columnSpanFull(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('number')->sortable()->searchable(),
+                Tables\Columns\TextColumn::make('customer.name')->sortable()->searchable(),
+                Tables\Columns\TextColumn::make('status')->sortable()->searchable(),
+                Tables\Columns\TextColumn::make('total_price')
+                    ->sortable()->searchable()
+                    ->summarize([
+                        Tables\Columns\Summarizers\Sum::make()->money(),
+                    ]),
+                Tables\Columns\TextColumn::make('created_at')->label('Order Date')->date(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\ActionGroup::make([
+                    Tables\Actions\ViewAction::make(),
+                    Tables\Actions\EditAction::make(),
+                    Tables\Actions\DeleteAction::make(),
+                ])
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListOrders::route('/'),
+            'create' => Pages\CreateOrder::route('/create'),
+            'edit' => Pages\EditOrder::route('/{record}/edit'),
+        ];
+    }
+}
