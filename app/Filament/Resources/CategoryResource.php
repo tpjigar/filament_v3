da0f49d9f7a6cd3433b362cb66542f9c668172c2
tpjigar <tp.jigar@gmail.com>
2023-09-25 19:29:08 +0530

    Category Product relationship

--- app/Filament/Resources/CategoryResource.php
+++ app/Filament/Resources/CategoryResource.php
@@ -101,7 +101,7 @@ class CategoryResource extends Resource
     public static function getRelations(): array
     {
         return [
-            //
+            RelationManagers\ProductsRelationManager::class,
         ];
     }
 
